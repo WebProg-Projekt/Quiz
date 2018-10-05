d3b29313584633e4e2b12a6d317a5d37fa343b7c
Beliz Balim <belizbalim94@gmail.com>
2018-10-05 09:57:09 +0200

    Punkt2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 import stylesheet from "./index.css";
 import theme from "./theme.css";
+
 import App from "./app.js";
 
-
-    // Erst loslaufen, wenn das Document Object Modul bereit ist
-    window.addEventListener("load", () => {
-        // Anwendung starten
-        let app = new App();
-        app.start();
-    });
+// Erst loslaufen, wenn das Document Object Modul bereit ist
+window.addEventListener("load", () => {
+    // Anwendung starten
+    let app = new App();
+    app.start();
+});
