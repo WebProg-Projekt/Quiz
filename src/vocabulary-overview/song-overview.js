3628824d54ac63fef25a2bf1de481a06e761b525
Beliz Balim <belizbalim94@gmail.com>
2018-10-08 18:58:22 +0200

    Namensaenderung

---
+++ src/vocabulary-overview/song-overview.js
@@ -0,0 +1,71 @@
+"use strict";
+
+import stylesheet from "./song-overview.css";
+
+/**
+ * View mit der Übersicht der vorhandenen Songs.
+ */
+class SongOverview {
+    /**
+     * Konstruktor,
+     * @param {Objekt} app Zentrales App-Objekt der Anwendung
+     */
+    constructor(app) {
+        this._app = app;
+    }
+
+    /**
+     * Von der Klasse App aufgerufene Methode, um die Seite anzuzeigen. Die
+     * Methode gibt daher ein passendes Objekt zurück, das an die Methode
+     * _switchVisibleContent() der Klasse App übergeben werden kann, um ihr
+     * die darzustellenden DOM-Elemente mitzuteilen.
+     *
+     * @return {Object} Darzustellende DOM-Elemente gemäß Beschreibung der
+     * Methode App._switchVisibleContent()
+     */
+
+    // src/song-overview/song-overview.js:
+    onShow() {
+        // Anzuzeigende HTML-Elemente ermitteln
+        let section = document.querySelector("#song-overview").cloneNode(true);
+
+        return {
+        className: "song-overview",
+        topbar: section.querySelectorAll("header > *"),
+        main: section.querySelectorAll("main > *"),
+        };
+
+        //Event Handler nach Single Page Router nicht mehr notwendig
+        // Event Handler registrieren
+        //let newSongItem = section.querySelector("header .item.new-song");
+
+        //newSongItem.addEventListener("click", () => {
+            //this._app.showSongDisplayEdit("", "new");
+        //})
+
+        // Ergebnis zurückliefern
+        //return content;
+    }
+
+    /**
+     * Von der Klasse App aufgerufene Methode, um festzustellen, ob der Wechsel
+     * auf eine neue Seite erlaubt ist. Wird hier true zurückgegeben, wird der
+     * Seitenwechsel ausgeführt.
+     *
+     * @param  {Function} goon Callback, um den Seitenwechsel zu einem späteren
+     * Zeitpunkt fortzuführen, falls wir hier false zurückgeben
+     * @return {Boolean} true, wenn der Seitenwechsel erlaubt ist, sonst false
+     */
+    onLeave(goon) {
+        return true;
+    }
+
+    /**
+     * @return {String} Titel für die Titelzeile des Browsers
+     */
+    get title() {
+        return "Übersicht";
+    }
+}
+
+export default SongOverview;
