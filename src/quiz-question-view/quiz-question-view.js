66a6be12035ea36b44e88c5d75cbcd239a3ec269
Beliz Balim <belizbalim94@gmail.com>
2018-10-10 10:30:34 +0200

    tiral

--- src/quiz-question-view/quiz-question-view.js
+++ src/quiz-question-view/quiz-question-view.js
@@ -14,6 +14,7 @@ class QuizQuestionView {
      */
     constructor(app) {
         this._app = app;
+        
     }
 
     /**
@@ -61,6 +62,8 @@ class QuizQuestionView {
 
     }
 
+
+    showNextSlide() {}
 
 }
 
