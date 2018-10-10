0336c0d0d8624e313e05cf56ddfde0ac0765f977
Beliz Balim <belizbalim94@gmail.com>
2018-10-10 11:40:31 +0200

    erste Frage erscheint

--- src/quiz-question-view/quiz-question-view.js
+++ src/quiz-question-view/quiz-question-view.js
@@ -4,9 +4,6 @@ import stylesheet from "./quiz-question-view.css";
 
 class QuizQuestionView {
 
-    //einige Fragen als Übergangslösüng, bis wir im Browser tatsaechlich Vokabeln speichern können
-
-
 
     /**
      * Konstruktor,
@@ -14,7 +11,6 @@ class QuizQuestionView {
      */
     constructor(app) {
         this._app = app;
-        
     }
 
     /**
@@ -29,6 +25,20 @@ class QuizQuestionView {
     onShow () {
         // Anzuzeigende HTML-Elemente ermitteln
         let section = document.querySelector("#quiz-question-view").cloneNode(true);
+
+        // Referenz über HTML Elemente für Fragen
+        let question = document.querySelector(".question");
+        let questionnr = document.querySelector(".question-number")
+
+        let questions = {
+                number: "1",
+                german: "der Hund",
+                english: "dog"
+        };
+
+        question.innerHTML = `${questions["german"]}` ;
+        questionnr.innerHTML = `${questions["number"]}`;
+
 
         return {
         className: "quiz-question-view",
@@ -58,10 +68,15 @@ class QuizQuestionView {
     }
 
     //Quiz erstellen, also 10 Fragen aussuchen
-    generateQuiz () {
-
+    /*_generateQuiz() {
+        let questions = {
+                number: "1",
+                german: "der Hund",
+                english: "dog"
+        }
+        return questions;
     }
-
+ */
 
     showNextSlide() {}
 
